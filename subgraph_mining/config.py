b11ea4ef0be209c47f994c8fe9940e07eeb73c67
Ruth Tamiru <ruthtamiru10@gmail.com>
2025-12-22 13:34:53 +0300

    Change search strategy from greedy to mcts

--- subgraph_mining/config.py
+++ subgraph_mining/config.py
@@ -79,7 +79,7 @@ def parse_decoder(parser):
         max_pattern_size=5,
         min_neighborhood_size=2,
         max_neighborhood_size=3,
-        search_strategy="greedy",
+        search_strategy="mcts",
         out_batch_size=3,
         node_anchored=True,
         memory_limit=1000000
